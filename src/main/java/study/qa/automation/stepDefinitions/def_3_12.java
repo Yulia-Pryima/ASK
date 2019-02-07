690d8a1203f0393d2c4af020e9b57c2114bbc6b8
AnaBK <lien82@gmail.com>
2019-02-07 13:09:30 -0500

    ana

--- src/main/java/study/qa/automation/stepDefinitions/def_3_12.java
+++ src/main/java/study/qa/automation/stepDefinitions/def_3_12.java
@@ -2,29 +2,106 @@ package study.qa.automation.stepDefinitions;
 
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
 
 public class def_3_12 {
-    @And("^Select Multiple-Choice Type of question$")
-    public void selectMultipleChoiceTypeOfQuestion() {
-    }
-
-    @And("^Type Text of Question \"([^\"]*)\"$")
-    public void typeTextOfQuestion(String arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
-    }
-
     @And("^Select Multiple-Choice Type of question (\\d+)$")
     public void selectMultipleChoiceTypeOfQuestion(int arg0) {
-        
     }
 
-    @And("^Select option (\\d+) and option (\\d+)  as correct answers$")
-    public void selectOptionAndOptionAsCorrectAnswers(int arg0, int arg1) {
-        
+    @And("^Type text of Question Question (\\d+)$")
+    public void typeTextOfQuestionQuestion(int arg0) {
+
+    }
+
+    @And("^Type text in Options Option (\\d+) and Option (\\d+)$")
+    public void typeTextInOptionsOptionAndOption(int arg0, int arg1) {
+
     }
 
     @And("^Select option (\\d+) and option (\\d+) as correct answers$")
     public void selectOptionAndOptionAsCorrectAnswers(int arg0, int arg1) {
+
     }
-}
+
+    @And("^Select Quiz Quizz (\\d+)$")
+    public void selectQuizQuizz(int arg0) {
+
+    }
+
+    @And("^Push Create New Assignment button$")
+    public void pushCreateNewAssignmentButton() {
+
+    }
+
+    @And("^Select student Ariel Mermaid$")
+    public void selectStudentArielMermaid() {
+
+    }
+
+    @Then("^Verify that quiz Quizz (\\d+) was assign$")
+    public void verifyThatQuizQuizzWasAssign(int arg0) {
+    }
+
+    @And("^Delete quiz with name Quizz (\\d+)$")
+    public void deleteQuizWithNameQuizz(int arg0) {
+
+    }
+
+    @And("^Delete assignments with name Quizz (\\d+)$")
+    public void deleteAssignmentsWithNameQuizz(int arg0) {
+
+    }
+
+    @And("^Push give Assignment button$")
+    public void pushGiveAssignmentButton() {
+
+    }
+
+    @And("^Click logout on left navigational panel$")
+    public void clickLogoutOnLeftNavigationalPanel() {
+
+
+    }
+
+    @And("Type email 1mustafalost2014d@rrqkd9t5fhvo5bgh.ga")
+    public void typeEmailMustafalostDRrqkdTFhvoBghGa(int arg0, int arg1, int arg2, int arg3, int arg4) {
+    }
+
+    @And("Type Password Test123*")
+    public void typePasswordTest(int arg0) {
+
+    }
+
+    @And("^Select option (\\d+) as correct answers$")
+    public void selectOptionAsCorrectAnswers(int arg0) {
+        
+    }
+
+    @And("^Type text in Options Option (\\d+)$")
+    public void typeTextInOptionsOption(int arg0) {
+        
+    }
+
+    @And("^Select student Anna Petrova$")
+    public void selectStudentAnnaPetrova() {
+        
+    }
+
+    @And("^Type text of Question Question$")
+    public void typeTextOfQuestionQuestion() {
+
+
+    }
+
+    @And("^Type text in Question* Question$null")
+    public void typeTextInQuestionQuestion() {
+    }
+
+    @And("^Type text in \"([^\"]*)\" Question$")
+    public void typeTextInQuestion(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+   }
--- src/main/resources/features/3.12.feature
+++ src/main/resources/features/3.12.feature
@@ -10,29 +10,29 @@ Feature: 3.12
     And Push Create new Quiz button
     And Type Title of the Quiz "Quizz 1"
     And Click Add Question
-    And Select Multiple-Choice Type of question 1
-    And Type text of Question "Question 1"
-    And Type text in Options "Option 1" and "Option 2"
-    And Select option 1 and option 2 as correct answers
+    And Select Single-Choice Type of question
+    And Type text of Question "Single choice Question"
+    And Type text in Options "Text of option 1" and "Text of option 2"
+    And Select first option as correct answer
     And Push Save button
     #  // Create Assignments
     And Click Assignments on left navigational panel
     And Push Create new Assignment button
     And Select Quiz "Quizz 1"
-    And Select Student "Ariel Mermaid*"
+    And Select Student "Anna Petrova"
     And Push Give Assignment button
     And Click Logout on left navigational panel
-    #  // Student "Ariel Mermaid*"
-    And Type email " 1mustafalost2014d@rrqkd9t5fhvo5bgh.ga"
+    #  // Student "Anna Petrova"
+    And Type email "xmu-housej@jhow.ml"
     And Type Password "Test123*"
     And Push Sign In button
     And Click My Assignments on left navigational panel
-    Then Verify that quiz "Quizz 1" was assign
+    Then Verify that quiz Quizz 1 was assign
     And Click Logout on left navigational panel
     #  // Login as teacher and delete Assignment
     And Type email "tanyasptest+3@gmail.com"
     And Type Password "winter2018"
     And Push Sign In button
-    And Delete quiz with name "Quizz 1"
-    And Delete assignments with name "Quizz 1"
+    And Delete quiz with name Quizz 1
+    And Delete assignments with name Quizz 1
     And Click Logout on left navigational panel
